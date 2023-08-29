cd02780bf320b19c00f61e86ad839dba6ca199f4
remicrobe <64744563+remicrobe@users.noreply.github.com>
2023-08-29 13:57:03 +0200

    Ajout composant news

---
+++ OpenDisk-APP/src/components/NewsComponents.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card
+    style="margin-left: 5%;"
+    max-width="344"
+  >
+    <v-img
+      src="@/assets/newsbackground.webp"
+      height="200px"
+      cover
+    ></v-img>
+
+    <v-card-title>
+      Informations !
+    </v-card-title>
+
+    <v-card-text>
+      Cette page aurait pu servir à afficher les informations relatives a l'actualité du projet, étant donné que l'API a un cout d'hebergement, ne sachant pas jusqu'à quand
+      je pourrais la maintenir, cette page d'info restera statique !
+    </v-card-text>
+
+
+
+
+  </v-card>
+</template>
--- OpenDisk-APP/src/views/Home.vue
+++ OpenDisk-APP/src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
-  <NavBar />
+  <NavBar style='z-index:20001' />
+  <NewsComponents/>
   <HelloPage />
 </template>
 
 <script lang="ts" setup>
+  import NewsComponents from '@/components/NewsComponents.vue';
   import NavBar from '@/components/NavigationBar.vue'
   import HelloPage from '@/components/HelloPage.vue'
 </script>
